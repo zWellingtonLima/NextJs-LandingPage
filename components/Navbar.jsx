e09ac8a2628aee89416bfe2d111425f73340d3e1
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-04-23 15:24:38 +0100

    Hero Section added.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { motion } from "framer-motion";
 
 import styles from "../styles";
@@ -15,6 +14,10 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img src="/search.svg" alt="search icon" className="w-[24px] h-[24px] object-contain"/>
+    <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
+      METAVERSUS
+    </h2>
+      <img src="/menu.svg" alt="menu icon" className="w-[24px] h-[24px] object-contain"/>
     </div>
   </motion.nav>
 );

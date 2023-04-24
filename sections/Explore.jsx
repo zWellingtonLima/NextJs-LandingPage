20c04f7f06b1f6a8c88bf03f42089b6b759efa45
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-04-24 20:04:21 +0100

    Get started Section and StartSteps Component added.

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -9,7 +9,7 @@ import { ExploreCard, TitleText, TypingText } from "../components";
 import { exploreWorlds } from "../constants";
 
 const Explore = () => {
-  const [active, setActive] = useState('world-2');
+  const [active, setActive] = useState('world-5');
 
   return (
     <section className={`${styles.paddings}`} id="explore">

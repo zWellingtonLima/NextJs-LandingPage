f544b805002766fa2a071d8b94c624dd489ea234
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-04-25 21:20:31 +0100

    NewFeatures component and WahtsNew section done.

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -5,7 +5,9 @@ const StartSteps = ({ number, text }) => (
     <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#424f5d]`}>
       <p className='font-bold text-[24px] text-white'>0{number}</p>
     </div>
-    <p className='flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]'></p>
+    <p className='flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]'>
+      {text}
+    </p>
   </div>
 );
 

56587388965a3c866876765968f58c42b2bb8746
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-04-26 14:00:50 +0100

    Section Feedback started and also the InsightCard component.

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { motion } from 'framer-motion'
+import styles from '../styles'
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className='flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative'>
+          <div className='feedback-gradient'/>
+          <div>
+            <h4 className='font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white'>Samantha</h4>
+            <p className='mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white'>Founder | Metaversus</p>
+          </div>
+
+          <p></p>
+      </motion.div>
+    </motion.div>
   </section>
 );
 

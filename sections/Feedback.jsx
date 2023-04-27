76eedff789e12b76cf19845e04ce8c625059d29b
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-04-27 11:43:23 +0100

    Feedback section done.

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -22,8 +22,23 @@ const Feedback = () => (
             <p className='mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white'>Founder | Metaversus</p>
           </div>
 
-          <p></p>
+          <p className='mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white'>
+           “With the development of today's technology, metaverse is very useful for today's work, or can be called web 3.0. by using metaverse you can use it as anything” 
+          </p>
       </motion.div>
+
+      <motion.div 
+        variants={fadeIn('left','tween', 0.2, 1)}
+        className='relative flex-1 flex justify-center items-center'>
+          <img src="/planet-09.png" alt="planet-09" className='w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]'/>
+
+          <motion.div
+            variants={zoomIn(0.4, 1)}
+            className='lg:block hidden absolute -left-[10%] top-[3%]'>
+              <img src="/stamp.png" alt="stamp" className='w-[155px] h-[155px] object-contain'/>
+          </motion.div>
+      </motion.div>
+
     </motion.div>
   </section>
 );
